ca1372b32c72e82913cd41c0176426b33322ef76
Emmanuel Olivo <emmanuel@mmacbook-con-dos-emes.local>
2018-01-27 16:39:34 -0600

    Add elements to ChannelView

---
+++ Smack/Smack/View/GradientView.swift
@@ -0,0 +1,17 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Emmanuel Olivo on 27/01/18.
+//  Copyright © 2018 ConDosEmes. All rights reserved.
+//
+
+import UIKit
+
+
+@IBDesignable
+class GradientView: UIView {
+
+    
+
+}
